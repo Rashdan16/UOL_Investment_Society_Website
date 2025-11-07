8458f2e5efd97b3ea5f7f81165b2b867a216cfd4
Rashdan Khan <rashdankhan16@gmail.com>
2025-11-07 22:49:19 +0000

    Initial Django project commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('events/', views.events, name='events'),
+    path('challenges/', views.challenges, name='challenges'),
+    path('socials/', views.socials, name='socials'),
+]
---
+++ main/views.py
@@ -0,0 +1,20 @@
+﻿from django.shortcuts import render
+
+# Homepage
+
+def home(request):
+    return render(request, 'main/index.html')
+
+# Simple pages
+
+def about(request):
+    return render(request, 'main/about.html', { 'page_title': 'About' })
+
+def events(request):
+    return render(request, 'main/events.html', { 'page_title': 'Events' })
+
+def challenges(request):
+    return render(request, 'main/challenges.html', { 'page_title': 'Challenges' })
+
+def socials(request):
+    return render(request, 'main/socials.html', { 'page_title': 'Socials' })
